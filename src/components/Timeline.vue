8840fae2da81bb3f2267541ef9af9805d6505e2c
tiago.s.cardoso <tiago.s.cardoso@animaeducacao.com.br>
2023-03-25 20:55:33 -0300

    :construction: adding suspense handling

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -4,7 +4,7 @@ import TimelineItem from "./TimelineItem.vue"
 import { periods } from "../constants"
 
 const postStore = usePosts()
-postStore.fetchPosts()
+await postStore.fetchPosts()
 </script>
 
 <template>
