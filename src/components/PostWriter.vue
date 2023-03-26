57d0b5767ce0e92220f3b5f555482f25bdebbf8e
tiago.s.cardoso <tiago.s.cardoso@animaeducacao.com.br>
2023-03-26 16:00:37 -0300

    :construction: creating a new post title section

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { TimelinePost } from "../post"
+
+const props = defineProps<{
+  post: TimelinePost
+}>()
+
+const title = ref(props.post.title)
+</script>
+
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">New Post</div>
+        <input type="text" class="input" v-model="title" />
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,1 +1,15 @@
-<template>New Post Route</template>
+<script setup lang="ts">
+import { DateTime } from "luxon"
+import PostWriter from "../components/PostWriter.vue"
+import { TimelinePost } from "../post"
+
+const post: TimelinePost = {
+  id: "-1",
+  title: "Title",
+  created: DateTime.now(),
+}
+</script>
+
+<template>
+  <PostWriter :post="post" />
+</template>
