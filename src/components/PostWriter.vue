e3b3aa212b9bd4368a6171844e7203bae047f010
tiago.s.cardoso <tiago.s.cardoso@animaeducacao.com.br>
2023-03-27 18:43:47 -0300

    :construction: building the writer section

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
 import { TimelinePost } from "../post"
 
 const props = defineProps<{
@@ -7,6 +7,23 @@ const props = defineProps<{
 }>()
 
 const title = ref(props.post.title)
+const content = ref(props.post.markdown)
+const html = ref("")
+const contentEditable = ref<HTMLDivElement>()
+
+onMounted(() => {
+  if (!contentEditable.value) {
+    throw Error("ContentEditable DOM node was not found")
+  }
+  contentEditable.value.innerText = content.value
+})
+
+function handleInput() {
+  if (!contentEditable.value) {
+    throw Error("ContentEditable DOM node was not found")
+  }
+  content.value = contentEditable.value?.innerText
+}
 </script>
 
 <template>
@@ -16,6 +33,16 @@ const title = ref(props.post.title)
         <div class="label">New Post</div>
         <input type="text" class="input" v-model="title" />
       </div>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <div contenteditable ref="contentEditable" @input="handleInput" />
+    </div>
+    <div class="column">
+      <div v-html="html" />
+      {{ content }}
     </div>
   </div>
 </template>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -7,6 +7,7 @@ const post: TimelinePost = {
   id: "-1",
   title: "Title",
   created: DateTime.now(),
+  markdown: "## Title",
 }
 </script>
 
