264ef58b69fa63eeb8aa0b80889e2ccbe2caad44
tiago.s.cardoso <tiago.s.cardoso@animaeducacao.com.br>
2023-03-28 17:43:28 -0300

    :construction: using body-parser to persist the new post on backend

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,22 @@
 import express from "express"
 import cors from "cors"
-import { today, thisWeek, thisMonth } from "../post"
+import bodyParser from "body-parser"
+import { today, thisWeek, thisMonth, Post } from "../post"
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
+
+const allPosts: Post[] = [today, thisWeek, thisMonth]
 
 app.get("/posts", (req, res) => {
-  res.json([today, thisWeek, thisMonth])
+  res.json(allPosts)
+})
+
+app.post<{}, {}, Post>("/posts", (req, res) => {
+  const post = { ...req.body, id: (Math.random() * 100000).toFixed() }
+  allPosts.push(post)
+  res.json(allPosts)
 })
 
 app.listen(8000, () => {
