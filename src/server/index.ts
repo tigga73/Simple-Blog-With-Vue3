de89d124e70d64410dbb8c6aebcc8ec7a64ae5e0
tiago.s.cardoso <tiago.s.cardoso@animaeducacao.com.br>
2023-03-31 12:21:20 -0300

    :sparkles: creating a endpoint to save the new user

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,12 +2,14 @@ import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
 import { today, thisWeek, thisMonth, Post } from "../post"
+import { NewUser, User } from "../users"
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
 const allPosts: Post[] = [today, thisWeek, thisMonth]
+const allUsers: User[] = []
 
 app.get("/posts", (req, res) => {
   res.json(allPosts)
@@ -17,6 +19,13 @@ app.post<{}, {}, Post>("/posts", (req, res) => {
   const post = { ...req.body, id: (Math.random() * 100000).toFixed() }
   allPosts.push(post)
   res.json(allPosts)
+})
+
+app.post<{}, {}, NewUser>("/users", (req, res) => {
+  const newUser = { ...req.body, id: (Math.random() * 100000).toFixed() }
+  allUsers.push(newUser)
+  const { password, ...rest } = newUser
+  res.json(rest)
 })
 
 app.listen(8000, () => {
