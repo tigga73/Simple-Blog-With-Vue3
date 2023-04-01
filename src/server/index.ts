1c1a05dd6a89aaaaa266bdd8668bfb615f918714
tiago.s.cardoso <tiago.s.cardoso@animaeducacao.com.br>
2023-04-01 14:38:17 -0300

    :construction: signing with jwt

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,29 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import jsonwebtoken from "jsonwebtoken"
 import { today, thisWeek, thisMonth, Post } from "../post"
 import { NewUser, User } from "../users"
 
 const app = express()
 app.use(cors())
+app.use(cookieParser())
 app.use(bodyParser.json())
 
 const allPosts: Post[] = [today, thisWeek, thisMonth]
 const allUsers: User[] = []
+
+const SECRET = "my-secret"
+const COOKIE = "vuejs-jwt"
+
+function authenticate(id: string, req: Request, res: Response) {
+  const token = jsonwebtoken.sign({ id }, SECRET, {
+    issuer: "vuejs-course",
+    expiresIn: "30 days",
+  })
+  res.cookie(COOKIE, token, { httpOnly: true })
+}
 
 app.get("/posts", (req, res) => {
   res.json(allPosts)
@@ -24,6 +38,7 @@ app.post<{}, {}, Post>("/posts", (req, res) => {
 app.post<{}, {}, NewUser>("/users", (req, res) => {
   const newUser = { ...req.body, id: (Math.random() * 100000).toFixed() }
   allUsers.push(newUser)
+  authenticate(newUser.id, req, res)
   const { password, ...rest } = newUser
   res.json(rest)
 })
